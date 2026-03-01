b903c41d8856cbc94481dfcfa12a61aea871fbd1
Dillon Hailey <181585418+Velarken@users.noreply.github.com>
2026-03-01 16:45:48 -0500

    added form autocomplete fields and init form handling

--- src/components/authComponents/Login.jsx
+++ src/components/authComponents/Login.jsx
@@ -3,15 +3,43 @@ import { useState } from "react";
 export function Login({
 
 }) {
+    const [username,setUsername] = useState('');
+    const [password,setUserPassword] = useState('');
+    function handleFormSubmit(e) {
+        e.preventDefault();
+        console.log('Username: ', username, 'Password: ', password)
+        // Send form data to API
+    }
+    return (
+        <form id="loginForm" className="form" onSubmit={(e) => handleFormSubmit(e)}>
+            <p className="form-desc">
+                Once logged in, you will be able to view all posts. Publishers may write 
+                and edit their own posts before publishing their post for others to view.
+                Any user may leave a comment on your post to get the conversation started.
+            </p>
+            <div className="form-line">
+                <label htmlFor="username">Username:</label>
+                <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} name="username" autoComplete="username"/>
+            </div>
+            <div className="form-line">
+                <label htmlFor="password">Password:</label>
+                <input type="password" value={password} onChange={(e) => setUserPassword(e.target.value)} name="password" autoComplete="current-password"/>
+            </div>
 
-    function handleFormSubmit() {
+            <button type="submit" >Log In</button>
+        </form>
+    )
+}
+export default Login;
+
+    /* function handleFormSubmit() {
         fetch('', {
             method: 'POST',
             body: JSON.stringify({ username: username, password: password})
         })
         .then(res => res.json())
         // .then() save token data to localStorage
-    }
+    } */
     /* 
     fetch('https://api.example.com/data', {
     method: 'POST', // or 'GET', 'PUT', etc.
@@ -26,25 +54,3 @@ export function Login({
     .then(data => console.log(data))
     .catch(error => console.error('Error:', error));
     */
-
-    return (
-        <form id="loginForm" className="form">
-            <p className="form-desc">
-                Once logged in, you will be able to view all posts. Publishers may write 
-                and edit their own posts before publishing their post for others to view.
-                Any user may leave a comment on your post to get the conversation started.
-            </p>
-            <div className="form-line">
-                <label htmlFor="username">Username:</label>
-                <input type="text" name="username"/>
-            </div>
-            <div className="form-line">
-                <label htmlFor="password">Password:</label>
-                <input type="password" name="password"/>
-            </div>
-
-            <button type="submit">Log In</button>
-        </form>
-    )
-}
-export default Login;
--- src/components/authComponents/Signup.jsx
+++ src/components/authComponents/Signup.jsx
@@ -3,9 +3,31 @@ import { useState } from "react";
 export function Signup({
 
 }) {
+    const [username,setUsername] = useState('');
+    const [firstName,setFirstName] = useState('');
+    const [lastName,setLastName] = useState('');
+    const [password,setPassword] = useState('');
+    const [confirmPassword,setConfirmPassword] = useState('');
 
+    function handleFormSubmit(e) {
+        e.preventDefault();
+        const userData = {
+            username,
+            firstName,
+            lastName,
+            password,
+            confirmPassword
+        }
+        if (userData.password === userData.confirmPassword) console.log('Both password entries match.')
+        if (userData.password !== userData.confirmPassword) {
+            console.log('Passwords do not match. Please check and try again.')
+            return
+        }
+        console.table(userData)
+        // send form data to API
+    }
     return (
-        <form id="signupForm" className="form">
+        <form id="signupForm" className="form" onSubmit={(e) => handleFormSubmit(e)}>
             <p className="form-desc">
                 Once logged in, you will be able to view all posts. Publishers may write 
                 and edit their own posts before publishing their post for others to view.
@@ -13,23 +35,23 @@ export function Signup({
             </p>
             <div className="form-line">
                 <label htmlFor="username">Username:</label>
-                <input type="text" name="username"/>
+                <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} name="username" autoComplete="username"/>
             </div>
             <div className="form-line">
                 <label htmlFor="firstname">First Name:</label>
-                <input type="text" name="firstname"/>
+                <input type="text" value={firstName} onChange={(e) => setFirstName(e.target.value)} name="firstname" autoComplete="given-name"/>
             </div>
             <div className="form-line">
                 <label htmlFor="lastname">Last Name:</label>
-                <input type="text" name="lastname"/>
+                <input type="text" value={lastName} onChange={(e) => setLastName(e.target.value)} name="lastname" autoComplete="family-name"/>
             </div>
             <div className="form-line">
                 <label htmlFor="password">Password:</label>
-                <input type="password" name="password"/>
+                <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} name="password" autoComplete="current-password"/>
             </div>
             <div className="form-line">
                 <label htmlFor="confirmPassword">Confirm Password:</label>
-                <input type="password" name="confirmPassword"/>
+                <input type="password" value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)} name="confirmPassword" autoComplete="off"/>
             </div>
 
             <button type="submit" className="submit-button">Sign Up</button>
