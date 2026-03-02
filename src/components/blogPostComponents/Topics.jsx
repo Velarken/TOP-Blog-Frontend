d89492f0da9e88fde14968d2802deb4aef261bdb
Dillon Hailey <181585418+Velarken@users.noreply.github.com>
2026-03-02 12:17:18 -0500

    added rendering logic for Topic

---
+++ src/components/blogPostComponents/Topics.jsx
@@ -0,0 +1,17 @@
+export function Topics({
+
+}) {
+
+    return (
+        <div className="topic-container">
+            
+        </div>
+    )
+}
+export default Topics;
+
+/* 
+    *** To Do ***
+    - Create render logic for all available topics
+        * Render <Topic /> component for each item in topics array
+*/
