84240d504ad3b87d06342eb4f66d9a1f8bb0dc5b
Dillon Hailey <181585418+Velarken@users.noreply.github.com>
2026-03-02 12:12:37 -0500

    init Nav component

---
+++ src/components/Nav.jsx
@@ -0,0 +1,14 @@
+export function Nav({
+
+}) {
+    return (
+        <div className="nav-container">
+            <h1>Blogify</h1>
+
+            <nav>
+                Nav links here
+            </nav>
+        </div>
+    )
+}
+export default Nav;
